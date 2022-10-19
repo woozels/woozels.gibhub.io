c4419e2622eabd6484fa93f3d042255699c66ba7
woozels <beatricekronby@gmail.com>
2022-10-19 19:30:01 -0400

    Help :)

---
+++ games.js
@@ -0,0 +1,8 @@
+function setup(){
+    createCanvas(600,600)
+    frameRate(30); //changes the framerate
+}
+
+function draw(){ //draw by default is 60 fps
+    background(0); //set the background to rgb(200,200,200)
+} 
